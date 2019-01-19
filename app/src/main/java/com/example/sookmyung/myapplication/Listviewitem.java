dd57befc763726abc4dd7e2a8cf7bf14dd0c19da
sookmyung <@>
2019-01-19 11:58:59 +0900

    second

---
+++ app/src/main/java/com/example/sookmyung/myapplication/Listviewitem.java
@@ -0,0 +1,35 @@
+package com.example.sookmyung.myapplication;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class Listviewitem {
+    private int icon;
+    private String name;
+    public int getIcon(){return icon;}
+    public String getName(){return name;}
+    public Listviewitem(int icon,String name){
+        this.icon=icon;
+        this.name=name;
+    }
+}public class ListviewActivity extends ActionBarActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listview);
+        ListView listView=(ListView)findViewById(R.id.listview);
+        ArrayList<Listviewitem> data=new ArrayList<>();
+        Listviewitem lion=new Listviewitem(R.drawable.lion, "Lion");
+        Listviewitem tiger=new Listviewitem(R.drawable.tiger,"Tiger");
+        Listviewitem dog=new Listviewitem(R.drawable.dog,"Dog");
+        Listviewitem cat=new Listviewitem(R.drawable.cat,"Cat");
+        data.add(lion);
+        data.add(tiger);
+        data.add(dog);
+        data.add(cat);
+        ListviewAdapter adapter=new ListviewAdapter(this,R.layout.listviewitem,data);
+        listView.setAdapter(adapter);
+    }
+}
--- app/src/main/java/com/example/sookmyung/myapplication/MainActivity.java
+++ app/src/main/java/com/example/sookmyung/myapplication/MainActivity.java
@@ -1,11 +1,9 @@
 package com.example.sookmyung.myapplication;
-
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
 
