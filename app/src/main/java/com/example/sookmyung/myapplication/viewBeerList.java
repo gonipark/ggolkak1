cc2d032ee6e717678dab2f85dedb5290578c6476
sookmyung <@>
2019-01-19 16:49:50 +0900

    listview3

--- app/src/main/java/com/example/sookmyung/myapplication/viewBeerList.java
+++ app/src/main/java/com/example/sookmyung/myapplication/viewBeerList.java
@@ -8,24 +8,36 @@ import java.util.ArrayList;
 
 public class viewBeerList extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_beer_list);
-        ListView listView=(ListView)findViewById(R.id.listview);
-        ArrayList<Listviewitem> data=new ArrayList<>();
-        Listviewitem lion=new Listviewitem(R.drawable.hogarden, "hogarden");
-        //  Listviewitem tiger=new Listviewitem(R.drawable.tiger,"Tiger");
-        //Listviewitem dog=new Listviewitem(R.drawable.dog,"Dog");
-        //Listviewitem cat=new Listviewitem(R.drawable.cat,"Cat");
-        data.add(lion);
-        //data.add(tiger);
-        // data.add(dog);
-        // data.add(cat);
-        ListviewAdapter adapter=new ListviewAdapter(this,R.layout.item,data);
+
+
+        ListView listView = (ListView) findViewById(R.id.listview);
+        ArrayList<Listviewitem> data = new ArrayList<>();
+        Listviewitem hogarden = new Listviewitem(R.drawable.hogarden, "hogarden");
+        Listviewitem budwiser = new Listviewitem(R.drawable.budwiser, "budwiser");
+        Listviewitem guinness = new Listviewitem(R.drawable.guinness, "guinness");
+        Listviewitem heineken = new Listviewitem(R.drawable.heineken, "heineken");
+        Listviewitem kirin_ichiban = new Listviewitem(R.drawable.kirin_ichiban, "kirin ichiban");
+        Listviewitem kozel = new Listviewitem(R.drawable.kozel, "kozel");
+        Listviewitem tsingtao = new Listviewitem(R.drawable.tsingtao, "tsingtao");
+        data.add(hogarden);
+        data.add(budwiser);
+        data.add(guinness);
+        data.add(heineken);
+        data.add(kirin_ichiban);
+        data.add(kozel);
+        data.add(tsingtao);
+
+        ListviewAdapter adapter = new ListviewAdapter(this, R.layout.item, data);
         listView.setAdapter(adapter);
+
+
     }
-    }
+}
 
 
 
