b568583d853a4f5d47d85d7e5ab7c2d9cf922309
sookmyung <@>
2019-01-19 14:47:06 +0900

    listview

--- app/src/main/java/com/example/sookmyung/myapplication/viewBeerList.java
+++ app/src/main/java/com/example/sookmyung/myapplication/viewBeerList.java
@@ -1,10 +1,7 @@
 package com.example.sookmyung.myapplication;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.support.v7.app.AppCompatActivity;
 
 public class viewBeerList extends AppCompatActivity {
 
@@ -14,4 +11,8 @@ public class viewBeerList extends AppCompatActivity {
         setContentView(R.layout.activity_view_beer_list);
     }
 }
+
+
+
+
 
